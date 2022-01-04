de59c301376a1442ecfad58a0d97f041dea8aaed
xq.jin <xq.jin@aftership.com>
2022-01-04 16:43:36 +0800

    feat: add style-align-self

---
+++ tests/lib/rules/react-native/style-align-self.js
@@ -0,0 +1,54 @@
+/**
+ * @fileoverview style-align-self
+ * @author luoxiu
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../../lib/rules/react-native/style-align-self"),
+  RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const parserOptions = {
+  ecmaVersion: 'latest',
+  sourceType: "module",
+  ecmaFeatures: {
+    jsx: true,
+  },
+};
+
+const ruleTester = new RuleTester({ parserOptions });
+ruleTester.run("react-native/style-align-self", rule, {
+  valid: [
+    "const a = { alignSelf: 'flex-start' }",
+    "const a = { 'alignSelf': 'flex-start' }",
+    "const a = { alignSelf: `flex-start` }",
+    "const a = { alignSelf: 'xq.jin' }",
+  ],
+
+  invalid: [
+    {
+      code: "const a = { alignSelf: 'start' }",
+      errors: [{ messageId: "doesNotSupport" }],
+    },
+    {
+      code: "const a = { 'alignSelf': 'start' }",
+      errors: [{ messageId: "doesNotSupport" }],
+    },
+    {
+      code: "const a = { alignSelf: `self-start` }",
+      errors: [{ messageId: "doesNotSupport" }],
+    },
+    {
+      code: "const v = <button style={{ alignSelf: 'self-start' }} />",
+      errors: [{ messageId: "doesNotSupport" }],
+    },
+  ],
+});
