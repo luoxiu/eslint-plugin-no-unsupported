ddcf2fe23c2feeebef5fc4de32ac893c15b59fc9
xq.jin <xq.jin@aftership.com>
2022-01-05 10:50:33 +0800

    feat: add style-cursor

--- README.md
+++ README.md
@@ -34,3 +34,4 @@ Configure it in `.eslintrc.js` or `.eslintrc.json`:
 | `no-unsupported/react-native/style-vm-vh` | react native does not support `vw` and `vh` |
 | `no-unsupported/react-native/style-align-self` | react native only supports `alignSelf: auto\|flex-start\|flex-end\|center\|baseline\|stretch` |
 | `no-unsupported/react-native/style-border-self` | react native only supports `borderStyle: solid|dotted|dashed` |
+| `no-unsupported/react-native/style-cursor` | react native does not support `cursor` |
---
+++ tests/lib/rules/react-native/style-cursor.js
@@ -0,0 +1,52 @@
+/**
+ * @fileoverview style-cursor
+ * @author luoxiu
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../../lib/rules/react-native/style-cursor"),
+  RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const parserOptions = {
+  ecmaVersion: 'latest',
+  sourceType: "module",
+  ecmaFeatures: {
+    jsx: true,
+  },
+};
+
+const ruleTester = new RuleTester({ parserOptions });
+ruleTester.run("react-native/style-cursor", rule, {
+  valid: [
+    // give me some code that won't trigger a warning
+    "const a = { cursor: 'xq.jin' }",
+  ],
+
+  invalid: [
+    {
+      code: "const a = { cursor: 'help' }",
+      errors: [{ messageId: "doesNotSupport" }],
+    },
+    {
+      code: "const a = { cursor: 'url(x.png), help' }",
+      errors: [{ messageId: "doesNotSupport" }],
+    },
+    {
+      code: "const a = { cursor: 'url(x.png) 1 1, help' }",
+      errors: [{ messageId: "doesNotSupport" }],
+    },
+    {
+      code: "const a = { cursor: 'url(x.png) 1 1, url(x.png) 1 1, help' }",
+      errors: [{ messageId: "doesNotSupport" }],
+    },
+  ],
+});
